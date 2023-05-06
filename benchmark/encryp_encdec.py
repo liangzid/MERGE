fix the bug of attentionmask and softmax

benchmark/encryp_encdec.py
@@ -40,7 +40,6 @@ from utils import softmax_2RELU, softmax_2QUAD, activation_quad, activation_newG
 from gpt import gptEmbeddings
 
 
-
 class EncDecFlatten(nn.Module):
     def __init__(self, config, timing):
         super(EncDecFlatten,self).__init__()
@@ -61,8 +60,7 @@ class EncDecFlatten(nn.Module):
         self.bos_one_hot=crypten.cryptensor(self.bos_one_hot)
 
         self.embeddings=gptEmbeddings(config,timing)
-        if config.device!="cpu":
-            self.embeddings.cuda(config.device)
+        self.embeddings.cuda(config.device)
 
         self.num_attention_heads = config.num_attention_heads
         self.attention_head_size = int(config.hidden_size / config.num_attention_heads)

benchmark/profile_encdrdecdr.py
@@ -167,9 +167,7 @@ print("init done")
 timing = defaultdict(float)
 
 if config.accelarate_type=="our" or config.accelarate_type=="onlyMM":
-    config.device="cpu"
     m = GPTBaseFlatten(config, timing)
-    m=nn.DataParallel(m,device_ids=[0,1])
     model = encrypt_model(m, GPTBaseFlatten,
                   (config, timing), input_ids).eval()
 else:
@@ -181,7 +179,7 @@ else:
 dec_ids = encrypt_tensor(input_ids[:,:2],config)
 input_ids = encrypt_tensor(input_ids,config)
 
-num=5
+num=1
 avg_t = defaultdict(float)
 res_ls=[]
 

nlg/transformersV4251/models/gpt2/gpt2_no_softmax.py
@@ -264,6 +264,10 @@ class GPT2Attention(nn.Module):
         if bs!=1:
             attn_weights.repeat(bs,1,1,1)
 
+        ## no softmax!!!
+        attn_weights = nn.functional.softmax(attn_weights, dim=-1)
+        # print("================")
+        # print(attn_weights[0,0,sl-1,:])
 
         # print(f"Attn before: {attn_weights}")
         if not self.is_cross_attention:
@@ -271,7 +275,9 @@ class GPT2Attention(nn.Module):
             query_length, key_length = query.size(-2), key.size(-2)
             # print(f"q length: {query_length}")
             # print(f"k length: {key_length}")
-            causal_mask = self.bias[:, :, key_length - query_length : key_length, :key_length].to(torch.bool)
+            causal_mask = self.bias[:, :, key_length - query_length :128, :128].to(torch.bool)
+            # causal_mask = self.bias[:, :, key_length - query_length : key_length, :key_length].to(torch.bool)
+
             # no softmax, then we use the mask value 0 rather -inf.
             # mask_value = torch.finfo(attn_weights.dtype).min
             mask_value = 0.
@@ -282,7 +288,7 @@ class GPT2Attention(nn.Module):
             mask_value = torch.full([], mask_value, dtype=attn_weights.dtype).to(attn_weights.device)
 
             # attention with no mask
-            # attn_weights = torch.where(causal_mask, attn_weights, mask_value)
+            attn_weights = torch.where(causal_mask, attn_weights, mask_value)
 
         # print(f"Attn after: {attn_weights}")
 
@@ -290,8 +296,6 @@ class GPT2Attention(nn.Module):
         if head_mask is not None:
             attn_weights = attn_weights * head_mask
 
-        ## no softmax!!!
-        # attn_weights = nn.functional.softmax(attn_weights, dim=-1)
 
         ## move here for retraining
         attn_weights=attn_weights[:,:,:sl,:sl]
@@ -310,7 +314,7 @@ class GPT2Attention(nn.Module):
 
         # print(attn_weights[0,0,0,:])
         
-        attn_weights=self.relu(attn_weights)
+        # attn_weights=self.relu(attn_weights)
 
         # ## normalization function
         # attn_weights=nn.functional.normalize(attn_weights,p=2,dim=-1)